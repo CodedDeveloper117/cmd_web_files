e590068e390ff793b16a7cbe38987889a9bb19a0
unknown <thecmdtechteam@gmail.com>
2022-05-09 21:19:28 +0100

    Finished Sign up page

--- sign_up/sign-up.js
+++ sign_up/sign-up.js
@@ -6,6 +6,43 @@ const choiceContent = document.querySelector('#choice_content');
 const techTrackPlaceholder = document.querySelector('#tech_track_placeholder')
 const techTrackInput = document.querySelector('#tech_track_input')
 
+
+const passwordTextField = document.getElementById("password_section")
+const emailTextField = document.getElementById("email_section")
+const form = document.querySelector("form")
+
+form.addEventListener('submit', (e) => {
+    
+})
+
+function registerPasswordVisiblityToggle() {
+    const toggleIcon = document.querySelector("#toggle");
+    const password = document.querySelector("#password");
+    console.log(toggleIcon)
+
+    toggleIcon.addEventListener('click', () => {
+        console.log('ciic3')
+        const type = password.getAttribute("type") === "password" ? "text" : "password";
+        password.setAttribute("type", type);
+        const icon = type == 'password' ? 'show' : 'hide'
+        const passwordToggleIcon = `<img src='../assets/password_${icon}.svg' />`
+        toggleIcon.innerHTML = ''
+        toggleIcon.innerHTML = passwordToggleIcon
+    });
+}
+
+function showError(error) {
+    const element = document.querySelector('.error_text')
+    element.textContent = error;
+}
+
+function removeError() {
+    const element = document.querySelector('.error_text');
+    element.textContent = ""
+}
+
+registerPasswordVisiblityToggle()
+
 let techTracks = [
     { isChecked: false, track: 'Android Development' },
     { isChecked: false, track: 'Cloud Computing' },
